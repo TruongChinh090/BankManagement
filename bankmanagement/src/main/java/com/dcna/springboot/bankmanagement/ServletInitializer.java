078abd312255bb6b2141b4927844f8c3b781c159
Truong Chinh <truongchinh92@hotmail.com>
2020-06-10 16:31:59 +0700

    Commit inital

---
+++ bankmanagement/src/main/java/com/dcna/springboot/bankmanagement/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dcna.springboot.bankmanagement;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BankmanagementApplication.class);
+	}
+
+}
---
+++ bankmanagement/src/test/java/com/dcna/springboot/bankmanagement/BankmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcna.springboot.bankmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
